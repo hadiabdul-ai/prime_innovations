547f947b0da308757589ac39f51ea1b240355541
Hadi Abdul <abdulhadi2122@gmail.com>
2024-11-19 13:59:19 -0600

    fixes

--- next.config.js
+++ next.config.js
@@ -5,10 +5,13 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   
+  // Add support for static export
+  output: 'export',
   
   // Set the base path to match your repository name
   basePath: '/prime_innovations', // Replace <repository-name> with your repo name
   
+  assetPrefix: '/prime_innovations/',
   // Optionally, add other Next.js config
 };
 
