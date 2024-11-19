f41c730999c304e24c00863692896f508dda4482
Hadi Abdul <abdulhadi2122@gmail.com>
2024-11-19 15:21:43 -0600

    remove gh pages

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  output: 'export',  // Enable static exports
-  basePath: '/prime_innovations', // Your repo name
-  assetPrefix: '/prime_innovations/',
-  images: {
-    unoptimized: true, // Required for static export
-  }
 };
 
 module.exports = withMDX(nextConfig);
