56ad78a367d76d053851ff565ebd3149b64ca40e
Hadi Abdul <abdulhadi2122@gmail.com>
2024-11-20 00:20:18 -0600

    add tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Prime Innovations" />
+        <meta property="og:description" content="Turning your ideas into reality" />
+        <meta property="og:image" content="https://www.primeinnovations.ca/images/logo-full.png" />
+        <meta property="og:url" content="https://www.primeinnovations.ca/" />
+        <meta property="og:type" content="website" />
+
+        {/* Apple Meta Tags */}
+        <link rel="apple-touch-icon" href="https://www.primeinnovations.ca/images/logo-full.png" />
+        <meta name="apple-mobile-web-app-title" content="Prime Innovations" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${inter.variable} font-inter antialiased bg-black text-slate-100 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
